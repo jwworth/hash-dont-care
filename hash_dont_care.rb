4e983e02079231505a3ae3ed8cce09201c9799c5
Jake Worth <jakeworth82@gmail.com>
2015-05-21 15:05:58 -0500

    Add key? aliases

--- hash_dont_care.rb
+++ hash_dont_care.rb
@@ -8,6 +8,13 @@ class HashDontCare < Hash
     super symbolize_key!(k)
   end
 
+  def key?(k)
+    super symbolize_key!(k)
+  end
+  alias_method :has_key?, :key?
+  alias_method :include?, :key?
+  alias_method :member?, :key?
+
   private
 
   def symbolize_key!(k)
--- test/test_hash_dont_care.rb
+++ test/test_hash_dont_care.rb
@@ -22,10 +22,21 @@ class TestHashDontCare < Minitest::Test
     assert_equal [:black, :white], rgb.keys
   end
 
-  def test_it_has_railsy_aliases
+  def test_it_has_a_sort_alias
     rgb = HashDontCare.new
     rgb.store('black', '#000000')
     assert_equal '#000000', rgb[:black]
+  end
+
+  def test_it_has_key_aliases
+    rgb = HashDontCare.new
+    rgb[:black] = '#000000'
+    result = 'black'
+
+    assert_equal true, rgb.key?(result)
+    assert_equal true, rgb.has_key?(result)
+    assert_equal true, rgb.include?(result)
+    assert_equal true, rgb.member?(result)
   end
 
   def test_sets_default_value
